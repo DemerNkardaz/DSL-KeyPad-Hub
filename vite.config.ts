659fb9459297f2f0415922738f884b51e07c591a
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-05-03 00:41:38 +0300

    Fix after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-	base: '/DSL-KeyPad-Hub/',
+	base: '/DSL-KeyPad-Docs/',
 	server: {
 		open: true
 	},
